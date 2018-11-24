b12e664b7ae70c5e6bd9b83bd95477bbca80a557
gen1321 <gen1321@mail.ru>
2018-11-24 16:24:27 +0300

    basic options implementation

--- apps/colossus/lib/dsl.ex
+++ apps/colossus/lib/dsl.ex
@@ -1,9 +1,11 @@
 defmodule Colossus.DSL do
-  def __on_definition__(env, :def, name, _args, _guards, _body) do
-    desc = Module.get_attribute(env.module, :desc) 
-    options = Module.get_attribute(env.module, :option) 
+  alias Colossus.Options
 
-    Module.put_attribute(env.module, :actions, {name, desc, options})
+  def __on_definition__(env, :def, name, _args, _guards, _body) do
+    desc = Module.get_attribute(env.module, :desc)
+    options = Module.get_attribute(env.module, :option)
+
+    Module.put_attribute(env.module, :actions, {name, %{description: desc, options: options}})
 
     Module.delete_attribute(env.module, :desc)
     Module.delete_attribute(env.module, :option)
@@ -30,15 +32,36 @@ defmodule Colossus.DSL do
         help
       end
 
-      def run([action | args]) do
-        apply(__MODULE__, String.to_atom(action), args)
+      def run([action, args], options) do
+        case Keyword.get(@actions, String.to_atom(action)) do
+          %{options: function_options} ->
+            options = Colossus.Options.handle_options(function_options, %{})
+            apply(__MODULE__, String.to_atom(action), [args, options])
+
+          _ ->
+            apply(__MODULE__, String.to_atom(action), args)
+        end
+      end
+
+      def run([action, args]) do
+        case Keyword.get(@actions, String.to_atom(action)) do
+          %{options: function_options} ->
+            options = Colossus.Options.handle_options(function_options, %{})
+            apply(__MODULE__, String.to_atom(action), [args, options])
+
+          _ ->
+            apply(__MODULE__, String.to_atom(action), args)
+        end
       end
 
       def help do
         IO.inspect(@actions)
-        for {action, desc, _} <- Enum.reject(@actions, fn {name, _, _} -> name == :run end) do
-          IO.puts "#{action} - #{desc}"
+
+        for {action, %{description: desc}} <-
+              Enum.reject(@actions, fn {name, _} -> name == :run end) do
+          IO.puts("#{action} - #{desc}")
         end
+
         :ok
       end
     end
---
+++ apps/colossus/lib/options.ex
@@ -0,0 +1,38 @@
+defmodule Colossus.Options do
+  def handle_options(function_options, passed_options) do
+    Enum.reduce(function_options, passed_options, fn (func_opt, acc_passed_options) ->
+      handle_option(func_opt, acc_passed_options)
+    end)
+  end
+
+  def handle_option(opt, passed_options) do
+    {key, specs} = opt
+    Enum.reduce(specs, passed_options, fn (spec, acc) ->
+      case spec do
+        {:required, true} ->
+          validate_required(opt, acc)
+          acc
+        {:default, value} ->
+          set_default_value(acc, key, value)
+      end
+    end)
+  end
+
+  defp validate_required({key, _value} = opt, passed_options) do
+    case Map.fetch(passed_options, key) do
+      {:ok, value} ->
+        value
+      _ ->
+        raise "required options #{key} missing"
+    end
+    opt
+  end
+
+  defp set_default_value(passed_options, key, value) do
+    if Map.has_key?(passed_options, key) do
+      passed_options
+    else
+      Map.put(passed_options, key, value)
+    end
+  end
+end
