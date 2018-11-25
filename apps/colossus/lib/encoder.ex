970907ae2f89743f22f54f69b9a95162856f3b1f
Dmitry Rubinstein <rubinsteindb@gmail.com>
2018-11-25 18:29:30 +0200

    Encoder fix

--- apps/colossus/lib/encoder.ex
+++ apps/colossus/lib/encoder.ex
@@ -1,5 +1,6 @@
 defmodule Colossus.Encoder do
-  @callback encode(any) :: {:ok, String.t()}
+  @callback encode(any) :: String.t()
+  @callback parse(String.t()) :: String.t()
 
   defmacro sigil_E(expr, opts) do
     handle_sigil(expr, opts, __CALLER__.line)
@@ -27,7 +28,7 @@ defmodule Colossus.Encoder do
 
   def default_encode_help_command_to_eex({key, desc, options}) do
     ~E"""
-    Command: <%= to_string(key) %> 
+    Command: <%= to_string(key) %>
     Description: <%= desc %>
     Options: <%= for {name, desc} <- options do %>
        <%= String.pad_trailing(to_string(name), 15) <> " # " <> to_string(desc) %>
