dad70cffce27f5c41a2560f8aed2001cb9c287c3
gen1321 <gen1321@mail.ru>
2018-11-25 14:31:37 +0300

    proper encoders swap

--- apps/colossus/lib/colossus.ex
+++ apps/colossus/lib/colossus.ex
@@ -1,7 +1,8 @@
 defmodule Colossus do
-  defmacro __using__(_opts) do
+  defmacro __using__(opts \\ []) do
     quote do
       import Colossus.DSL
+      import Colossus.Encoder
       Module.register_attribute(__MODULE__, :desc, [])
       Module.register_attribute(__MODULE__, :option, accumulate: true)
       Module.register_attribute(__MODULE__, :module_option, accumulate: true)
@@ -10,8 +11,8 @@ defmodule Colossus do
       @on_definition Colossus.DSL
       @before_compile Colossus.DSL
 
-      @help_encoder &Colossus.Encoder.default_encode_help_to_eex/1
-      @help_command_encoder &Colossus.Encoder.default_encode_help_command_to_eex/1
+      @help_encoder unquote(Keyword.get(opts, :help_encoder, &Colossus.Encoder.default_encode_help_to_eex/1))
+      @help_command_encoder unquote(Keyword.get(opts, :help_command_encoder, &Colossus.Encoder.default_encode_help_command_to_eex/1))
     end
   end
 end
